c60ab4418bf350b9dcb4a8b720c9e1b30ef94cdc
jackendoff <jackendoff@163.com>
2019-01-03 18:31:49 +0800

    验证失败

--- bqex_api.py
+++ bqex_api.py
@@ -122,43 +122,157 @@ class BqexApi(object):
 
     # 获取签名,请求方式，排序参数
     def sign_in(self,method,url,params):
-        url_str = parse.urlencode(params, 'utf-8')
-        url_str = parse.quote_plus(method.upper() + str(url) + '?' + url_str)
-        no_base64 = (hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.sha256).hexdigest())
-        data = base64.b64encode(no_base64.encode())
+        # url_str = parse.urlencode(params, 'utf-8')
+        url_str = 'api_key='+AppKey+'&uid='+str(UID)+'&secret_key='+AppSecret
+        print(url_str)
 
-        return data
+        # url_str = parse.quote_plus(method.upper() + str(url) + '?' + url_str)
+        # print(url_str)
+        # no_base64 = (hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.md5).hexdigest())
+        no_base64 = hashlib.md5(url_str.encode("utf-8")).hexdigest()
+        print(no_base64)
+        # data = base64.b64encode((hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.sha256).hexdigest()).encode())
+        return no_base64.upper()
 
     # 下单
+    def order(self):
+        pass
     # 撤单
+
     # 获取深度
-    def auth_deepin(self):
-        url = 'https://www.bqex.pro/polarisex/authlib/deepin'
-        secret = 'GET/authlib/deepin?Action=pay&AppKey='+self.api_key +'&secret_key=' + self.secret_key+'&symbol=ethltc'+'&uid=' + self.uid
-        sign = self.sign_in(secret)
+    def authlib_deepin(self):
+        path = '/authlib/account'
+        method = 'get'
         params = {
-            'uid': self.uid,
-            'api_key': self.api_key,
+            'api_key': AppKey,
+            'uid': UID,
+            'secret_key': AppSecret,
+            'symbol': 'eth_ltc',
+
+        }
+        # sign = self.sign_in(method=method, url=path, params=params)
+        url_str = 'api_key=' + AppKey + '&uid=' + str(UID) + '&symbol=eth_ltc&secret_key=' + AppSecret
+        sign = hashlib.md5(url_str.encode("utf-8")).hexdigest()
+
+        # print(sign)
+        # app_key = parse.urlencode(AppKey)
+        # print(app_key)
+        params_data = {
+            'uid': UID,
+            'api_key': AppKey,
             'sign': sign,
-            'symbol':'ethltc'
+            'symbol': 'eth_ltc',
+
         }
         headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
-            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036"}
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            "Authorization": sign
+        }
 
-        data = requests.get(url, params=params, headers=headers)
+        # url = API_BASE+path+'?'+parse.urlencode(params_data)
+        # url = API_BASE+path+'?'+'api_key='+AppKey+'&uid='+str(UID)+'&sign='+sign
+        url = API_BASE + path
+        # print(url)
+        data = requests.post(url=url, data=params_data, headers=headers)
+        # print(data)
         data = data.content.decode()
         return data
+
+
     # 获取深度
 
     # 获取账号
     def get_auth(self):
-        pass
+        path = '/authlib/account'
+        method = 'get'
+        params = {
+            'api_key': AppKey,
+            'uid': UID,
+            'secret_key':AppSecret
+        }
+        sign = self.sign_in(method=method,url=path,params=params)
+        print(sign)
+        # app_key = parse.urlencode(AppKey)
+        # print(app_key)
+        params_data = {
+            'uid': UID,
+            'api_key':AppKey,
+            'sign':sign,
+        }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            "Authorization": sign
+        }
+
+        # url = API_BASE+path+'?'+parse.urlencode(params_data)
+        # url = API_BASE+path+'?'+'api_key='+AppKey+'&uid='+str(UID)+'&sign='+sign
+        url = API_BASE+path
+        print(url)
+        data = requests.post(url=url,data=params_data,headers=headers)
+        print(data)
+        data = data.content.decode()
+        return data
     # 获取未成交单
     # 交易单成状态
+
     # 成交记录
+    def trade_order(self):
+        path = '/authlib/tradeOrder'
+        url_str = 'api_key='+AppKey+'&uid='+str(UID)+'&symbol=eth_ltc&secret_key='+AppSecret
+        print(url_str)
+        sign = hashlib.md5(url_str.encode("utf-8")).hexdigest().upper()
+        params = {
+            'uid': UID,
+            'api_key': AppKey,
+            'sign': sign,
+            'symbol':'FCC'
+        }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            "Authorization": sign
+        }
+        url = API_BASE+path
+        data = requests.post(url=url,data=params,headers=headers)
+        data = data.content.decode()
+        return data
+
+
     # 获取所有交易对
+    def authlib_pairs(self):
+        path = '/authlib/pairs'
+        url_str = 'api_key='+AppKey+'&uid='+str(UID)+'&secret_key='+AppSecret
+        # print(url_str)
+        sign = hashlib.md5(url_str.encode("utf-8")).hexdigest().upper()
+        params = {
+            'uid':UID,
+            'api_key':AppKey,
+            'sign':sign
+        }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            # "Authorization": sign
+        }
+        url = API_BASE+path
+        print(url)
+        data = requests.post(url=url,data=params,headers=headers)
+        data = data.content.decode()
+        return data
+
     # 获取所有币种
+    def coin_coins(self):
+        url = 'https://www.bqex.pro/polarisex/coin/coins'
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            # "Authorization": sign
+        }
+        data = requests.post(url,headers=headers)
+        data = data.content.decode()
+        return data
 
 
 if __name__ == '__main__':
--- text.py
+++ text.py
@@ -3,6 +3,8 @@ import random
 import base64
 import hashlib
 import hmac
+import requests
+from content_safe import *
 from urllib import parse
 
 
@@ -32,19 +34,42 @@ sort_params = {
     "Timestamp" : 1465185768,
     "amount": 1,
     "diallingCode": "86",
-    "note": "note",
-    "phone": "18180980860"
+    "phone": "17630636381"
 }
-
+# https://api.bqex.pro/open/v1/pay/receipt/order
 url_str = parse.urlencode(sort_params,'utf-8')
 print(url_str)
-url_str = parse.quote_plus('POST'+'/open/v1/pay/receipt/order'+'?'+url_str)
+url_str = parse.quote_plus('GET'+'/accound/vcode'+'?'+url_str)
 print(url_str)
 
-AppSecret = 'ASIDZwO4MejWZK81boBTqEU6hy3Ad6D4JFHbRm4HjugGMoQe2a2HgcZ7CJJkxTy7DBjS'
-url_str = 'POST%2Fopen%2Fv1%2Fpay%2Freceipt%2Forder%3FAction%3Dpay%26AppKey%AKIDcZVSyyl8O2nzsFrKNsyj5sD1eCfJlRCI%26Nonce%3D123456%26SignatureMethod%3DHmacSHA256%26Timestamp%3D1465185768000%26amount%3D1%26diallingCode%3D86%26note%3Dnote%26phone%3D18180980860'
+# AppSecret = 'ASIDZwO4MejWZK81boBTqEU6hy3Ad6D4JFHbRm4HjugGMoQe2a2HgcZ7CJJkxTy7DBjS'
+# url_str = 'POST%2Fopen%2Fv1%2Fpay%2Freceipt%2Forder%3FAction%3Dpay%26AppKey%AKIDcZVSyyl8O2nzsFrKNsyj5sD1eCfJlRCI%26Nonce%3D123456%26SignatureMethod%3DHmacSHA256%26Timestamp%3D1465185768000%26amount%3D1%26diallingCode%3D86%26note%3Dnote%26phone%3D18180980860'
 print(url_str)
 no_base64 = (hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.sha256).hexdigest())
 print(no_base64)
-data = base64.b64encode(no_base64.encode())
+sign = base64.b64encode(no_base64.encode())
+print(sign)
+url = 'https://api.bqex.pro/open/v1/accound/vcode'+'?'+parse.urlencode(sort_params)
+print(url)
+headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
+            "Cookie": "Pycharm-26c2d973=dbb9b300-2483-478f-9f5a-16ca4580177e; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1512607763; Pycharm-26c2d974=f645329f-338e-486c-82c2-29e2a0205c74; _xsrf=2|d1a3d8ea|c5b07851cbce048bd5453846445de19d|1522379036",
+            "Authorization": sign
+        }
+data = requests.get(url=url,headers=headers,params=sort_params)
+data = data.content.decode()
 print(data)
+
+
+# def sign_in(self, method, url, params):
+#     # url_str = parse.urlencode(params, 'utf-8')
+#     url_str = 'api_key=' + AppKey + '&uid=' + str(UID) + '&secret_key=' + AppSecret
+#     print(url_str)
+#
+#     # url_str = parse.quote_plus(method.upper() + str(url) + '?' + url_str)
+#     # print(url_str)
+#     # no_base64 = (hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.md5).hexdigest())
+#     no_base64 = hashlib.md5(url_str.encode("utf-8")).hexdigest()
+#     print(no_base64)
+#     # data = base64.b64encode((hmac.new(AppSecret.encode("UTF-8"), url_str.encode("utf-8"), hashlib.sha256).hexdigest()).encode())
+#     return no_base64.upper()
