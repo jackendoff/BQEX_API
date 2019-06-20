af642cceb0a1a88ea84128fd547658c878c6e712
jackendoff <jackendoff@163.com>
2019-06-20 16:31:20 +0800

    添加biforex

---
+++ content_safe.py
@@ -0,0 +1,25 @@
+# AppKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALe40+7LBhoUP8wpb6Y0SE+QZCX2PZX+7wFoDmvbaJCdFs74nnbV3hJnySyttCFlrrsa5EHlX9izLzI8u/w/ndUCAwEAAQ=='
+# AppSecret = 'MIIBVwIBADANBgkqhkiG9w0BAQEFAASCAUEwggE9AgEAAkEAt7jT7ssGGhQ/zClvpjRIT5BkJfY9lf7vAWgOa9tokJ0WzviedtXeEmfJLK20IWWuuxrkQeVf2LMvMjy7/D+d1QIDAQABAkEArUpw3LQzNTYUVwZegW6fJi0TyZrRzEGCPtlfgOybXIqzcR/21JsdawbDcn3aEfioeBAuEi3gesiD6DlnLtSgoQIhAPTJoTbhaWyvVf8BM+UKEDETi845ZUJlL5LnPSLRwt8LAiEAwCMlFFc6vRkazryVFvO9izwuFmNLhprefj2O/WsLAp8CIQCetVfkGDPdNrUf96yo1GR5HT4owAfKC4bTzT9TIwMgzwIhALAfBxwwIW25F3OuNdPGNVbuISjweAi46X46m1n8xyjtAiEAmfDIfe9UPkOdbnard8kZkcK5Lie4M81wS3VU6jmXdoo='
+# UID = 110348
+#
+AppSecret = 'MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAgTjjv9sU/c9xDMwjaoRNYmhnz/8yJyJLFvgGiLdwzs7C3AKINdQoKrF0e0xFOIZZqzZt/wAGMb3Vt7aBNIuUcwIDAQABAkBXHhp1IucxAeROY2XRDub43Dh4wqx2M6l6D7af/0P/N+zKgg54lR3V/962DmbI1IKZvvNqI/ls68Y/RZk29U1BAiEA4T1wPgBXZKYf5tAQGE7582UiV/L4BwoDdPevNDtNu2ECIQCS3pjR5xWOngAhHf31oS26O/zeSMG7/x83maqyYlxUUwIhAI/7nFKmGTen0mDTxFYltjZXAhtCoDavugH9iyxSdKBBAiAEtn3i5m5rqChK58J5elWUT05694y38qTWzOiMVtmtnwIgEZlSIWBdb2CGR0+ANqCSNVeqlguviVH/hBbkvyeA4x0='
+AppKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAIE447/bFP3PcQzMI2qETWJoZ8//MiciSxb4Boi3cM7OwtwCiDXUKCqxdHtMRTiGWas2bf8ABjG91be2gTSLlHMCAwEAAQ=='
+UID = 222494
+
+# API_BASE = 'https://www.bqex.pro'
+# API_BASE = 'https://www.bqtt.pro'
+# API_BASE = 'https://www.bqg11n.com'
+API_BASE = 'https://www.bqex.tw'
+
+# api文档
+# url = 'https://www.bqopen.com/page/docs/exchange'
+
+
+
+# API_BASE = 'https://api.bqex.pro/open/v1'
+
+
+# API_BASE = 'http://testexapi.bqopen.com/open/v1'
+
+# AppKey = 'AKIDcZVSyyl8O2nzsFrKNsyj5sD1eCfJlRCI'
+# AppSecret =  'ASIDZwO4MejWZK81boBTqEU6hy3Ad6D4JFHbRm4HjugGMoQe2a2HgcZ7CJJkxTy7DBjS'
